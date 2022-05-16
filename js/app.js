b74bf419811da6e4a62b5cf068868698a54cd216
David Navarrete <dn4dn@icloud.com>
2022-05-17 01:20:18 +0200

    some bugs fixed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-let x;
-x = document.querySelector('.titles:nth-child(1)').textContent;
+let x = document.querySelector('body');
+x = x.children[0];
+x = x.children[4];
+x = x.children[1];
+x = x.children[1];
+x = x.children[0];
+x = x.children[0];
+x.src = 'img/mark.png'
+
+
 console.log(x);
