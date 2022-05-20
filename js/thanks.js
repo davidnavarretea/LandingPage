9a564730e3f0d5da075497030825452aebc6406e
David Navarrete <dn4dn@icloud.com>
2022-05-20 07:43:06 +0200

    JS Split

--- js/thanks.js
+++ js/thanks.js
@@ -1,7 +1,12 @@
 // Sign In Variables
 const modal = document.querySelector('.modalcontainer');
 const button = document.querySelector('.nav .button');
+const button2 = document.querySelector('.thankbutton .button')
 // Button Modal
 button.addEventListener('click', () => {
+    modal.classList.toggle('modalhidden');
+})
+// Button 2 Modal
+button2.addEventListener('click', () => {
     modal.classList.toggle('modalhidden');
 })
